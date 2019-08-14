b254137aa48dd7f158304eef82482865e28b8fb4
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-08-14 13:26:40 -0400

    Frontend code started

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
+//import Patron from "../abis/Patron.json";
+import PatronFactory from "../abis/PatronFactory.json";
+import getWeb3 from "../utils/getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  
+  constructor (props) {
+    super(props);
+    this.state = {
+      web3: null, 
+      account: null, 
+      contract: null 
+    };
+
+    this.createAccount = this.createAccount.bind(this);
+  }
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
+      // Use web3 to get the user's current account
       const accounts = await web3.eth.getAccounts();
-
+      const account = accounts[0];
+      
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = PatronFactory.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        PatronFactory.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, account, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,17 +47,11 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  createAccount = async () => {
+    const { account, contract } = this.state;
+    console.log('Create Account method ');
+    const response = await contract.methods.deploy(account).send( { from: account });
+    console.log(response);
   };
 
   render() {
@@ -54,17 +60,7 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <button onClick={ this.createAccount }>Create Account</button>
       </div>
     );
   }
