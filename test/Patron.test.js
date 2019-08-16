7e0db4003cf0b5ceee98bef97f9d46205e772a4c
Shailendra Shukla <shailenbdrashuklapro@gmail.com>
2019-08-16 01:09:47 -0400

    Added sample tests

--- test/Patron.test.js
+++ test/Patron.test.js
@@ -5,3 +5,25 @@
  * https://github.com/shailshukla96
  *
  */
+
+ /* eslint-disable no-undef */
+const patron = artifacts.require('./Patron.sol');
+
+contract('Patron', ([deployer, seller, buyer]) => {
+    let patron;
+
+    before(async() => {
+        patron = await patron.deployed();
+    });
+
+    describe('Deployment', async() => {
+        it('Should deploy successfully', async() => {
+            const address = await patron.address;
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, null);
+            assert.notEqual(address, '');
+            assert.notEqual(address, undefined);
+        });
+    });
+
+});
